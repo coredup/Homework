c8cd4697864fb47494a285dcfcc1c75744a4a061
IGOR <coreduo@rambler.ru>
2022-11-26 08:40:41 +0300

    Решили задачку 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿// 15 задачка 
+Console.WriteLine("Задачка номер 15");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("( выходной 6 - суббота , 7 - воскресенье) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Это не день недели (Дни 1-понедельник,2-вторник,3-среда,4-четверг,5-пятница,6-суббота,7-воскресенье");
+  }
+  else Console.WriteLine("(выходной 6 - суббота и 7 - воскресенье) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
