106effb67dc4cca0249138cbcd9d5fdf9f09fa83
IGOR <coreduo@rambler.ru>
2022-11-26 08:19:29 +0300

    Домашняя работа пример 2

---
+++ Example_3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.Read());
+if(a%2==0)Console.WriteLine("2,4,6,8");
+else Console.WriteLine("2,4");
+Console.ReadKey();
---
+++ Example_4/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 5;
+int b = 10;
+
+int min = a;
+if (b<min) min =b;
+Console.WriteLine(min);
